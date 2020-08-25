73beac01cc3a3d338b2a45bc797579218144cc5b
ebenezerak <ebenezerak@gmail.com>
2020-08-25 15:08:16 -0400

    Javascript assignments added to repo

---
+++ quiz_app/question.js
@@ -0,0 +1,72 @@
+var questions =[{
+    "question": "Which of the following can't be done with client-side JavaScript?",
+    "option1":"Sending a form's contents by email", 	
+    "option2":"Storing the form's contents to a database file on the server",
+    "option3":"Validating a form",	
+    "option4":"None of the above",
+    "answer": "2" 
+},{ 
+    "question": "What is the correct JavaScript syntax to write “Hello World”?",
+    "option1":"System.out.println()", 	
+    "option2":"println ()",
+    "option3":"document.write()",	
+    "option4":"response.write()",
+    "answer": "3" 
+},{ 
+    "question": "Which of the following is not considered a JavaScript operator?",
+    "option1":"new", 	
+    "option2":"this",
+    "option3":"delete",	
+    "option4":"typeof",
+    "answer": "2" 
+},{ 
+    "question": "Which one of the following is not used to generate dynamic web pages ?",
+    "option1":"PHP", 	
+    "option2":"ASP.NET",
+    "option3":"JSP",	
+    "option4":"none of the above",
+    "answer": "4" 
+},{
+    "question": "Which class provides an interface for invoking JavaScript methods and examining JavaScript properties.",
+    "option1":"ScriptObject", 	
+    "option2":"JSObject",
+    "option3":"JavaObject",	
+    "option4":"Jobject",
+    "answer": "2" 
+},{
+    "question": "How can you get the total number of arguments passed to a function?",
+    "option1":"Using args.length property", 	
+    "option2":"Using arguments.length property",
+    "option3":"Both of the above.",	
+    "option4":"None of the above.",
+    "answer": "2" 
+},{
+    "question": "Which of the following is correct about callbacks?",
+    "option1":"A callback is a plain JavaScript function passed to some method as an argument or option.", 	
+    "option2":"Some callbacks are just events, called to give the user a chance to react when a certain state is triggered.",
+    "option3":"Both of the above.",	
+    "option4":"None of the above.",
+    "answer": "3" 
+},{
+    "question": "Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+    "option1":"toSource()", 	
+    "option2":"valueOf()",
+    "option3":"toString()",	
+    "option4":"None of the above.",
+    "answer": "2" 
+},{
+    "question": "Which of the following function of String object causes a string to be displayed in the specified size as if it were in a <font size = 'size'> tag?",
+    "option1":"fixed()", 	
+    "option2":"fontcolor()",
+    "option3":"fontsize()",	
+    "option4":"bold()",
+    "answer": "3" 
+},{
+    "question": "Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?",
+    "option1":"pop()", 	
+    "option2":"push()",
+    "option3":"reduce()",	
+    "option4":"reduceRight()",
+    "answer": "4" 
+}             
+]
